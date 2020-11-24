7608c2961109a9d5440758771637b66bd1689916
Viktoryia <kulinkovichva@gmail.com>
2020-11-24 23:32:47 +0300

    created app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            image: '',
+            quote: '',
+        };
+        this.fetchSimpsonQuote = this.fetchSimpsonQuote.bind(this);
+    }
 
-export default App;
+    fetchSimpsonQuote() {
+        const url = 'https://simpsons-quotes-api.herokuapp.com/quotes';
+        axios
+            .get(url)
+            .then((response) => response.data)
+            .then((data) => {
+                this.setState({
+                    name: data[0].character,
+                    image: data[0].image,
+                    quote: data[0].quote,
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.fetchSimpsonQuote();
+    }
+
+    render() {
+        return (
+            <div className="QuotesContainer">
+                <button className="showBtn" onClick={this.fetchSimpsonQuote}>
+                    Show quote
+                </button>
+                <h1>{this.state.name}</h1>
+                <img src={this.state.image} />
+                <p>{this.state.quote}</p>
+            </div>
+        );
+    }
+}
